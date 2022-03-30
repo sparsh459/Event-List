f37746f1fa9f04892af01de5f7058441f87893c3
Sparsh Saxena <86906056+sparsh459@users.noreply.github.com>
2022-03-31 00:01:20 +0530

    Update filter.py

--- bookingsystem/filter.py
+++ bookingsystem/filter.py
@@ -4,12 +4,7 @@ import django_filters
 from bookingsystem.models import events
 
 class eventFilter(django_filters.FilterSet):
-    # EventCity = django_filters.CharFilter(field_name='City')
-    # EventDate = django_filters.CharFilter(field_name='Date')
-    # EventDate = django_filters.CharFilter(field_name='Date')
-    # EventPrice = django_filters.CharFilter(field_name='Price')
-    # EventCategory = django_filters.CharFilter(field_name='Category')
-
+    
     class Meta:
         model = events
         fields = '__all__'
